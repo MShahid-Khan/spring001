868cf2e3be52eb61c0348ab9c37d02398557473e
Shahzad Masud <shahzadmasud@lap-nfs087.netsolpk.com>
2018-11-15 20:18:57 +0500

    Travis File addition

--- opd/src/main/java/med/objava/spring/controller/LoginController.java
+++ opd/src/main/java/med/objava/spring/controller/LoginController.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 import med.objava.spring.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
